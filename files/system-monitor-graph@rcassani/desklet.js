d1c8e88e9fb1317c34608e305ea7cf3966b0dd23
Raymundo Cassani <raymundo.cassani@gmail.com>
2020-05-23 10:27:32 -0400

    Convert to 4-space indentation

--- files/system-monitor-graph@rcassani/desklet.js
+++ files/system-monitor-graph@rcassani/desklet.js
@@ -12,29 +12,29 @@ function main(metadata, desklet_id) {
 }
 
 MyDesklet.prototype = {
-  __proto__: Desklet.Desklet.prototype,
+    __proto__: Desklet.Desklet.prototype,
 
-  _init: function(metadata, desklet_id) {
-    Desklet.Desklet.prototype._init.call(this, metadata, desklet_id);
+    _init: function(metadata, desklet_id) {
+        Desklet.Desklet.prototype._init.call(this, metadata, desklet_id);
 
-    // initialize settings
-    this.settings = new Settings.DeskletSettings(this, this.metadata["uuid"], desklet_id);
-    this.settings.bindProperty(Settings.BindingDirection.IN, "type", "type", this.on_setting_changed);
+        // initialize settings
+        this.settings = new Settings.DeskletSettings(this, this.metadata["uuid"], desklet_id);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "type", "type", this.on_setting_changed);
 
-    // initialize desklet GUI
-    this.setupUI();
-  },
+        // initialize desklet GUI
+        this.setupUI();
+    },
 
-  setupUI: function(){
-    var type = this.type;
+    setupUI: function(){
+        var type = this.type;
 
-    // creates container for one child
-    this.window = new St.Bin();
-    // creates a label with text
-    this.text = new St.Label({text: "Hello Desktop ".concat(type)});
-    // adds label to container
-    this.window.add_actor(this.text);
-    // Sets the container as content actor of the desklet
-    this.setContent(this.window);
-  },
+        // creates container for one child
+        this.window = new St.Bin();
+        // creates a label with text
+        this.text = new St.Label({text: "Hello Desktop ".concat(type)});
+        // adds label to container
+        this.window.add_actor(this.text);
+        // Sets the container as content actor of the desklet
+        this.setContent(this.window);
+    },
 };
